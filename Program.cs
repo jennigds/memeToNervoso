31fd245101a2f03873c909c2583b86ebe7a8594a
Jennigds <jennifergds89@gmail.com>
2023-04-17 22:40:28 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string fraseEscrita, fraseMEMEtoNervoso;
+Console.WriteLine("Ei, o que você pensa sobre mim?");
+fraseEscrita = Console.ReadLine()!;
+
+fraseMEMEtoNervoso = $"{fraseEscrita.Remove(10)}... NÃO, PERA.\nTô nervoso";
+Console.WriteLine(fraseMEMEtoNervoso);
